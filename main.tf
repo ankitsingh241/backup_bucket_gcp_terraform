5443150dc369e6b1257ab834b47b738e540eaa6c
Ankit_Singh <iamankitsingh24@gmail.com>
2024-06-27 16:25:14 +0530

    changed bucket name

--- main.tf
+++ main.tf
@@ -2,10 +2,5 @@ resource "google_storage_bucket" "multi_bucket" {
   name          = var.bucket_name
   location      = "asia-south1"
   project       = var.projectid
-}
-
-resource "google_storage_bucket" "multi_bucket"{
-  name          = var.bucket_name2
-  location      = "asia-south1"
-  project       = var.projectid2
+  count         = 2
 }
--- variables.tf
+++ variables.tf
@@ -4,16 +4,6 @@ variable "projectid" {
 }
 
 variable "bucket_name" {
-    type = string
-    default = "gcp_bucket"
-}
-
-variable "projectid2" {
-    type = string
-    default = "fiery-chess-417809"
-}
-
-variable "bucket_name2" {
-    type = string
-    default = "backup_gcp_bucket"
+    type = list()
+    default = ["gcp_bucket", "backup_gcp_bucket"]
 }
