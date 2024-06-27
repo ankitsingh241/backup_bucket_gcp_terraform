694db0da3b67123fb3eae8164995cd345f0abf20
Ankit_Singh <iamankitsingh24@gmail.com>
2024-06-27 16:34:58 +0530

    changed bucket name

--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@ resource "google_storage_bucket" "multi_bucket" {
   name          = var.bucket_name
   location      = "asia-south1"
   project       = var.projectid
-  count         = lenght(var.bucket_name)
+  count         = length(var.bucket_name)
 }
